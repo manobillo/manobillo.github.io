cbf9e0a3fab4e70a7bef8d661c6eb2396c93253e
manobillo <mkhan238@depaul.edu>
2024-10-01 20:26:37 -0500

    debug butterfly

--- src/components/FractalTree.js
+++ src/components/FractalTree.js
@@ -25,47 +25,19 @@ const Sketch = (p5) => {
     const dx = 0.2;
     let xoff = 0;
 
+    // Define colorvar here for access in both loops
+    let colorvar = p5.frameCount;
+
     p5.beginShape();
 
+    // First loop
     for (let a = -p5.PI / 2; a <= p5.PI / 2; a += da) {
       let n = p5.noise(xoff, t);
       r = p5.sin(2 * a) * p5.map(n, 0, 1, 50, 325);
       let x = r * p5.cos(a);
       let y = r * p5.sin(a);
-      let colorvar = p5.frameCount;
       xoff += dx;
       let rColor = p5.map(colorvar, 0, p5.width, p5.random(230), 255);
       let gColor = p5.map(colorvar, 0, p5.width, p5.random(210), 255);
       let bColor = p5.map(colorvar, 0, p5.height, p5.random(250), 255);
-      p5.stroke(rColor, gColor, bColor, 15);
-      p5.vertex(x, y);
-    }
-
-    for (let a = p5.PI / 2; a <= 3 * p5.PI / 2; a += da) {
-      let n = p5.noise(xoff, t);
-      r = p5.sin(2 * a) * p5.map(n, 0, 1, 50, 325);
-      let x = r * p5.cos(a);
-      let y = r * p5.sin(a);
-      xoff -= dx;
-      let rColor = p5.map(colorvar, 0, p5.width, p5.random(230), 255);
-      let gColor = p5.map(colorvar, 0, p5.width, p5.random(210), 255);
-      let bColor = p5.map(colorvar, 0, p5.height, p5.random(250), 255);
-      p5.stroke(rColor, gColor, bColor, 15);
-      p5.vertex(x, y);
-    }
-
-    t += 0.01;
-    if (p5.frameCount % 500 === 1) {
-      p5.background(0);
-    }
-    p5.endShape(p5.CLOSE);
-  };
-};
-
-const FractalTree = () => (
-  <div id="fractal-tree">
-    <ReactP5Wrapper sketch={Sketch} />
-  </div>
-);
-
-export default FractalTree;
+     
