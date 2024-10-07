df3f940ef6eb4a41d6c44f48a973c1ddab719382
Maheen Khan <mkhan238@depaul.edu>
2024-10-07 17:33:40 -0500

    stupid comma missing

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -89,6 +89,23 @@ const JobList = () => {
         "Studied multiple clinical data study protocols to produce regulatory compliant data for analysis and support FDA submission of clinical cancer trials.",
         "Acted as the point of contact for daily study activities, collaborating with internal teams and external sponsors to resolve data issue, by implementing hands-on solutions to data management challenges by implementing Python scripts for automated query optimization."
       ]
+    },
+    "HealthStream": {
+      jobTitle: "Software Developer @",
+      duration: "JAN 2021 - JAN 2022",
+      desc: [
+        "Transformed web applications to a responsive design for mobile and tablet support by practicing Agile and Scrum into the daily workflow.",
+        "Worked with SQL, IIS, and C# simultaneously for new feature developments."
+      ]
+    },
+    "ComplyALIGN": {
+      jobTitle: "Technical Support Tier 2 @",
+      duration: "FEB 2015 - JAN 2021",
+      desc: [
+        "Provided help desk support for clients over the phone.",
+        "Developed and installed graphics on client websites, as well as maintained the network and domain infrastructure.",
+        "Took part in QA testing of product releases."
+      ]
     }
   };
 
